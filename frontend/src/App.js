54246d61c79d6941ecafea7d374e3c13f65a0c59
alxBL85 <alexander.bl85@gmail.com>
2021-04-24 17:33:57 -0600

    feat: installed material-ui and included input text field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Box component="span" m={1}>
+ <div>
+      Developer: Bacilio Alexander Bolaños Lima <br/>
+      Email: alexander.bl85@gmail.com
+      <hr/>
+
+      <form className={"inputSearchIssue"} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Search Issue" variant="outlined" />
+      </form>
     </div>
+</Box>
+
+
+    
   );
 }
 
